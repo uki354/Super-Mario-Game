0bcc77a59dc9d453de44647c5ad44bde51b909ea
uki <uros.3.stevanovic@gmail.com>
2022-05-21 19:02:16 +0200

    Add ground, bricks and pipes to box2d world

--- core/src/com/uki/mariobros/screen/PlayScreen.java
+++ core/src/com/uki/mariobros/screen/PlayScreen.java
@@ -8,16 +8,20 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.uki.mariobros.MarioBros;
 import com.uki.mariobros.scene.Hud;
 
 public class PlayScreen  implements Screen {
-
 
     private MarioBros game;
     private OrthographicCamera gameCam;
@@ -26,6 +30,10 @@ public class PlayScreen  implements Screen {
     private TmxMapLoader mapLoader;
     private TiledMap map;
     private OrthogonalTiledMapRenderer renderer;
+
+    private World world;
+    private Box2DDebugRenderer b2dr;
+
 
 
 
@@ -39,10 +47,72 @@ public class PlayScreen  implements Screen {
         renderer = new OrthogonalTiledMapRenderer(map);
         gameCam.position.set(viewport.getWorldWidth() / 2 , viewport.getWorldHeight() / 2 , 0 );
 
+        world = new World(new Vector2(0, 0), true);
+        b2dr = new Box2DDebugRenderer();
+
+
+        BodyDef bDef = new BodyDef();
+        PolygonShape shape = new PolygonShape();
+        FixtureDef fDef = new FixtureDef();
+        Body body;
+
+        for (MapObject  object : map.getLayers().get(2).getObjects().getByType(RectangleMapObject.class)){
+            Rectangle  rect = ((RectangleMapObject) object).getRectangle();
+
+            bDef.type = BodyDef.BodyType.StaticBody;
+            bDef.position.set(rect.getX() + rect.getWidth() / 2, rect.getY() + rect.getHeight()  / 2 );
+
+            body = world.createBody(bDef);
+            shape.setAsBox(rect.getWidth() / 2, rect.getHeight() /2);
+            fDef.shape = shape;
+            body.createFixture(fDef);
+
+        }
+
+        for (MapObject  object : map.getLayers().get(3).getObjects().getByType(RectangleMapObject.class)){
+            Rectangle  rect = ((RectangleMapObject) object).getRectangle();
+
+            bDef.type = BodyDef.BodyType.StaticBody;
+            bDef.position.set(rect.getX() + rect.getWidth() / 2, rect.getY() + rect.getHeight()  / 2 );
+
+            body = world.createBody(bDef);
+            shape.setAsBox(rect.getWidth() / 2, rect.getHeight() /2);
+            fDef.shape = shape;
+            body.createFixture(fDef);
+
+        }
+
+        for (MapObject  object : map.getLayers().get(5).getObjects().getByType(RectangleMapObject.class)){
+            Rectangle  rect = ((RectangleMapObject) object).getRectangle();
+
+            bDef.type = BodyDef.BodyType.StaticBody;
+            bDef.position.set(rect.getX() + rect.getWidth() / 2, rect.getY() + rect.getHeight()  / 2 );
+
+            body = world.createBody(bDef);
+            shape.setAsBox(rect.getWidth() / 2, rect.getHeight() /2);
+            fDef.shape = shape;
+            body.createFixture(fDef);
+
+        }
+
+        for (MapObject  object : map.getLayers().get(4).getObjects().getByType(RectangleMapObject.class)){
+            Rectangle  rect = ((RectangleMapObject) object).getRectangle();
+
+            bDef.type = BodyDef.BodyType.StaticBody;
+            bDef.position.set(rect.getX() + rect.getWidth() / 2, rect.getY() + rect.getHeight()  / 2 );
+
+            body = world.createBody(bDef);
+            shape.setAsBox(rect.getWidth() / 2, rect.getHeight() /2);
+            fDef.shape = shape;
+            body.createFixture(fDef);
+
+        }
+
     }
 
     @Override
     public void show() {
+
     }
 
     public void update(float time){        
@@ -68,6 +138,8 @@ public class PlayScreen  implements Screen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         renderer.render();
+
+        b2dr.render(world, gameCam.combined);
 
         game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
         hud.stage.draw();
